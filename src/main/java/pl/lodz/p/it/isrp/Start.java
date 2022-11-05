b98bed7d4003c7551fa239f83a900f744a6df947
Szymon Ziemecki <szymonj60@gmail.com>
2022-11-05 18:41:02 +0100

    done

--- src/main/java/pl/lodz/p/it/isrp/Start.java
+++ src/main/java/pl/lodz/p/it/isrp/Start.java
@@ -1,5 +1,9 @@
 package pl.lodz.p.it.isrp;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Rozwiązanie problemu sortowania tablicy (zawiera błędy które należy usunąć z
  * zastosowanie nadzorowanego wykonania programu (debug)). Diagnostykę należy
@@ -7,7 +11,18 @@ package pl.lodz.p.it.isrp;
  */
 public class Start {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
+        String connectionUrl = System.getenv("DATABASE_CONNECTION_URL") + System.getenv("DATABASE_NAME");
+        String dbUser = System.getenv("DATABASE_USER");
+        String dbPassword = System.getenv("DATABASE_PASSWORD");
+        try {
+            Connection conn2 = DriverManager.getConnection(connectionUrl, dbUser,dbPassword);
+            if (conn2 != null) {
+                System.out.println("Connected to database");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         if (args.length == 0) {
             System.out.println("Brak podanej liczby całkowitej jako argumentu wywołania");
             System.exit(1);
